5d150cfb1ce3c85a5288a7eadc913aa261aad12c
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-30 07:53:57 -0700

    Removes configuraiton requirementc

--- src/pypline/structures.py
+++ src/pypline/structures.py
@@ -1,4 +1,5 @@
 import time
+# import IPython
 
 class In:
     def __init__(self, origin, transform_fn):
@@ -28,14 +29,14 @@ class Sink:
         self.buffer_size = buffer_size
 
     def __has_space__(self):
-        return len(self.data) < self.buffer_size
+        return self.buffer_size >= len(self.data) 
 
     def __push__(self,d):
         self.data.append(d)
 
     def __flush__(self):
         d = self.data
-        self.data = []
+        self.data.clear()
         return d
 
 
@@ -46,19 +47,21 @@ class Source:
     def __has_data__(self):
         if len(self.data) < 1:
             return False
+        else:
+            return True
 
     def __pull__(self):
-        return self.data.pop()
+        return self.data.pop(0)
 
 class Pipeline:
-    def __init__(self, config):
-        self.id = config.id
-        self.source = config.source
-        self.delay_sec = config.delay_sec
-        self.sink = config.sink
-        self.pipe_in = config.pipe_in
-        self.pipe_out = config.pipe_out
-        self.logger = config.logger
+    def __init__(self, id, source,sink, pipe_in, pipe_out, logger=None,delay_sec=0,):
+        self.id = id
+        self.source = source
+        self.delay_sec = delay_sec
+        self.sink = sink
+        self.pipe_in = pipe_in
+        self.pipe_out = pipe_out
+        self.logger = logger
 
     def __log__(self, msg, params):
         if self.logger != None:
@@ -72,27 +75,36 @@ class Pipeline:
     def start(self):
         self.__log__(f"Pipeline {self.id} started", {})
 
-        has_data = True
         counter = 0
 
-        while has_data:
+        while True:
             counter += 1
             self.__log__(f"Pipeline working... operation {counter}", {})
 
             self.__delay__()
 
+            # IPython.embed()
             val = self.pipe_in.pull()
 
             has_data = (val != False)
+
+            # Exit if no data present
+            if not has_data:
+                self.__log__(f"Source input exhausted", {})
+                break
 
             result = self.pipe_out.push(val)
 
             if result == False:
                 self.__log__(f"Sink buffer exhausted, flushing...", {})
+                # Flush the buffer
                 self.sink.__flush__()
+                # Re-push the value
+                self.pipe_out.push(val)
 
             self.__log__(f"Operation {counter} complete", {})
 
+        self.sink.__flush__()
         self.__log__(f"Pipeline {self.id} finished", {})
 
     # Easter egg method
